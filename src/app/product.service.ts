a0da8e0b0a6902bacf28c620c77bbb76b05ccac1
iamadoctorforreal <52762924+iamadoctorforreal@users.noreply.github.com>
2020-10-11 12:28:54 +0100

    a check

---
+++ src/app/product.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import {Http} from '@angular/http';
+import {Response} from '@angular/http'
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl:  '../assets/album.json';
+  getAlbum(id) {
+    Number;
+  }
+  constructor(private _http: Http) { }
+
+}
